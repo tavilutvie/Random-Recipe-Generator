846b1bbd863267793cfdaeec8dd4a3eff698c941
Tavi <tavirazin@gmail.com>
2022-10-19 00:50:55 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Details from "./component/Details";
 import Header from "./component/Header";
 import Main from "./component/Main";
