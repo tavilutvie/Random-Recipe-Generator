40913ce5037570b08d38f11cd5a0802134608874
Tavi <tavirazin@gmail.com>
2022-11-07 20:37:55 +0700

    Update SearchArea.jsx

--- src/pages/SearchArea.jsx
+++ src/pages/SearchArea.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
   
-  export default function SearchArea() {
+export default function SearchArea() {
 
     const { id } = useParams();
     console.log("id is: " + id);
@@ -10,54 +10,51 @@ import { Link, useParams } from "react-router-dom";
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
 
-
-
     useEffect(() => {
         console.log(url);
         fetch(url)
-          .then((response) => {
+        .then((response) => {
             console.log(response);
             if (response.status === 200) {
-              return response.json();
+            return response.json();
             }
-          })
-          .then((data) => {
+        })
+        .then((data) => {
             console.log(data);
             setData(data.meals);
-          })
-          .catch((error) => {
+        })
+        .catch((error) => {
             console.error(error);
             setError(error);
-          });
-      }, [url]);
+        });
+    }, [url]);
 
     return (
-      <div className="p-10">
-        <p className="pb-10 text-center font-medium text-xl">{id}</p>
-        {error ? <h2>Error fetching data</h2> : null}
+        <div className="p-10">
+            <p className="pb-10 text-center font-medium text-xl">{id}</p>
+            {error ? <h2>Error fetching data</h2> : null}
             {data ? (
-      <ul className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 ">
-        {data.map((file) => (
-          <li key={file.strMealThumb} className="relative">
-            <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
-            <Link
-                  to={"/Details/" + file.idMeal}
-                  className="hover:bg-indigo-700"
-                >
-              <img src={file.strMealThumb} alt="" className="object-cover pointer-events-none group-hover:opacity-75" />
-              <button type="button" className="absolute inset-0 focus:outline-none">
-                <span className="sr-only">View details for {file.strMeal}</span>
-              </button>
-              </Link>
-            </div>
-            <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">{file.strMeal}</p>
-            {/* <p className="block text-sm font-medium text-gray-500 pointer-events-none">{file.idMeal}</p> */}
-          </li>
-        ))}
-      </ul>
-      ) : (
-        <p>Loading...</p>
-    )}
-      </div>
+                <ul className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 ">
+                    {data.map((file) => (
+                        <li key={file.strMealThumb} className="relative">
+                            <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+                                <Link
+                                to={"/Details/" + file.idMeal}
+                                className="hover:bg-indigo-700">
+                                    <img src={file.strMealThumb} alt="" className="object-cover pointer-events-none group-hover:opacity-75" />
+                                    <button type="button" className="absolute inset-0 focus:outline-none">
+                                        <span className="sr-only">View details for {file.strMeal}</span>
+                                    </button>
+                                </Link>
+                            </div>
+                            <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">{file.strMeal}</p>
+                            {/* <p className="block text-sm font-medium text-gray-500 pointer-events-none">{file.idMeal}</p> */}
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
     )
-  }
+}
